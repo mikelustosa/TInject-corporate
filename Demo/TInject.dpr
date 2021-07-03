84b375344dc25c44c37803c333bcd934da1a1095
Mike <mikelustosa@gmail.com>
2021-07-03 19:04:36 -0300

    Init

---
+++ Demo/TInject.dpr
@@ -0,0 +1,39 @@
+program TInject;
+
+uses
+  Vcl.Forms,
+  Windows,
+  uTInject.ConfigCEF,
+  u_principal in 'u_principal.pas' {frmPrincipal};
+
+{$R *.res}
+
+begin
+  {##########################################################################################
+  Colocar arquivos CEFLib junto a pasta binária da aplicação (Nao definir ou passar vazio)
+  Caso deseja informar.. segue exemplo abaixo
+  ##########################################################################################}
+
+//  GlobalCEFApp.PathLogFile          := '';
+//  GlobalCEFApp.PathFrameworkDirPath := 'C:\Componentes\WhatsApp\Comp\BIN';
+//  GlobalCEFApp.PathResourcesDirPath := 'C:\Componentes\WhatsApp\Comp\BIN';
+//  GlobalCEFApp.PathLocalesDirPath   := 'C:\Componentes\WhatsApp\Comp\BIN\locales';
+//  GlobalCEFApp.Pathcache            := 'C:\Componentes\WhatsApp\Comp\BIN\Cache';
+//  GlobalCEFApp.PathUserDataPath     := 'C:\Componentes\WhatsApp\Comp\BIN\User Data';
+
+  //Forma 1 GlobalCEFApp.Pathxx       := '';                      //Irá procura procurar o Arquivo PADRAO no mesmo local do EXE
+  //Forma 2 GlobalCEFApp.Pathxx       := 'C:\Componentes\demo\bin'; //<-  NOME do ARQUIVO INFORMADO
+  //Forma 3 GlobalCEFApp.Pathxx       := 'BIN';                     //<-  NOME do ARQUIVO INFORMADO
+  //Forma 4 GlobalCEFApp.Pathx         := '..\Source\;              //<-  NOME do ARQUIVO INFORMADO
+  //Exemplo se aplica para todos os PATH
+
+
+  If not GlobalCEFApp.StartMainProcess then
+     Exit;
+
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+
+end.
