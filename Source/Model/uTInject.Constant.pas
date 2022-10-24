730da7107807f05467508771e2b3570762b6a8f9
Mike <mikelustosa@gmail.com>
2022-10-24 13:38:46 -0300

    Update uTInject.Constant.pas

--- Source/Model/uTInject.Constant.pas
+++ Source/Model/uTInject.Constant.pas
@@ -65,7 +65,7 @@ Const
   FrmConsole_JS_GetAllContacts          = 'window.WAPI.getAllContacts();';
   FrmConsole_JS_GetBatteryLevel         = 'window.WAPI.getBatteryLevel();';
   FrmConsole_JS_GetMyNumber             = 'getMyNumber();';
-  FrmConsole_JS_GetUnreadMessages       = 'window.WAPI.getUnreadMessages(true, true, true);';
+  FrmConsole_JS_GetUnreadMessages       = 'window.WAPI.getUnreadMessages(includeMe="True", includeNotifications="True", use_unread_count="True");';
   FrmConsole_JS_GetAllChats             = 'window.WAPI.getAllChats();';
   FrmConsole_JS_checkDelivered          = 'window.WAPI.getDelivered();';
   FrmConsole_JS_WEBmonitorQRCode        = 'var AQrCode = document.getElementsByTagName("canvas")[0].toDataURL("image/png");console.log(JSON.stringify({"name":"getQrCodeWEB","result":{AQrCode}}));';
